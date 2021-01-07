e11d65d8944af477836a88ef59a52e315c5ec959
Caroline Ruud <caroline.ruud@outlook.com>
2021-01-07 21:17:25 +0100

    updated

--- ewallet/src/App.vue
+++ ewallet/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!-- <button v-on:click="router-link to="/addcard" type="button">Add new card</button>-->
-    </div>
+    <div id="nav"></div>
     <router-view />
   </div>
 </template>
@@ -12,7 +10,6 @@ export default {};
 </script>
 
 <style lang="scss">
-//@import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css");
 @import url("https://fonts.googleapis.com/css2?family=Dosis:wght@300&display=swap");
 #app {
   font-family: Dosis, sans-serif;
